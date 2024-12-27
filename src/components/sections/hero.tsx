7c792890d124238e7c117aaf980f27e8c13190fc
Recinos <00182122@uca.edu.sv>
2024-12-27 12:32:25 -0600

    some basic config

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import { MapPin } from 'lucide-react';
 
 import SagarHeadshot from "../../../public/images/sagar-headshot.jpg";
-import test from 'public/images/test.png';
+import pfp from 'public/images/pfp_2024_no_bg.png';
 import SocialIcons from '@/components/data-display/social-icons';
 import Typography from '@/components/general/typography';
 import Container from '@/components/layout/container';
@@ -15,7 +15,7 @@ const HeroSection = () => {
         <div className="flex items-center justify-center md:order-last md:flex-grow md:justify-end">
           <div className="relative h-[300px] w-[280px] md:h-[360px] md:w-[320px]">
             <Image
-              src={SagarHeadshot}
+              src={pfp}
               alt="Headshot of Sagar"
               className="absolute z-10 h-[280px] w-[240px] border-8 border-gray max-md:left-5 md:left-0 md:top-0 md:h-[320px] md:w-[280px]"
               style={{ objectFit: 'cover' }}
@@ -28,21 +28,17 @@ const HeroSection = () => {
         <div className="flex max-w-3xl flex-grow flex-col justify-center gap-8 md:order-first md:items-start md:justify-center 2xl:gap-12">
           <div className="flex flex-col gap-2">
             <Typography variant="h1">
-              Hi, I&apos;m Sagar{' '}
-              <span className="inline-block animate-waving-hand">👋</span>
+              Hi, I&apos;m Recinos{''}
+              
             </Typography>
             <Typography>
-              I&apos;m a full stack developer (React.js & Node.js) with a focus
-              on creating (and occasionally designing) exceptional digital
-              experiences that are fast, accessible, visually appealing, and
-              responsive. Even though I have been creating web applications for
-              over 7 years, I still love it as if it was something new.
+              I make things work
             </Typography>
           </div>
           <div className="flex flex-col gap-2">
             <div className="flex gap-2">
               <MapPin className="stroke-gray-600" />
-              <Typography>Ahmedabad, India</Typography>
+              <Typography>El Salvador</Typography>
             </div>
 
             <div className="flex items-center gap-2">
