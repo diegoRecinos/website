9905d9d4301ffcf529fb6691c072baa7287c11db
Recinos <00182122@uca.edu.sv>
2024-12-28 12:23:18 -0600

    img

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -3,6 +3,9 @@ import { MapPin } from 'lucide-react';
 
 import pfpgithub from 'public/images/pfp-github.jpg';
 import pfp from 'public/images/pfp_2024_no_bg.png';
+import pfpIcon from 'public/images/pfp-icon.png';
+import originalPfp from 'public/images/original_crop.jpg';
+
 import SocialIcons from '@/components/data-display/social-icons';
 import Typography from '@/components/general/typography';
 import Container from '@/components/layout/container';
@@ -15,7 +18,7 @@ const HeroSection = () => {
         <div className="flex items-center justify-center md:order-last md:flex-grow md:justify-end">
           <div className="relative h-[300px] w-[280px] md:h-[360px] md:w-[320px]">
             <Image
-              src={pfpgithub}
+              src={originalPfp}
               alt="Headshot of Sagar"
               className="absolute z-10 h-[280px] w-[240px] border-8 border-gray max-md:left-5 md:left-0 md:top-0 md:h-[320px] md:w-[280px]"
               style={{ objectFit: 'cover' }}
@@ -41,15 +44,6 @@ const HeroSection = () => {
               <Typography>El Salvador</Typography>
             </div>
 
-            <div className="flex items-center gap-2">
-              <div className="flex h-6 w-6 items-center justify-center">
-                <span className="relative flex h-3 w-3">
-                  <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-emerald-400 opacity-75"></span>
-                  <span className="relative inline-flex h-3 w-3 rounded-full bg-emerald-500"></span>
-                </span>
-              </div>
-              <Typography>Available</Typography>
-            </div>
           </div>
           <SocialIcons />
         </div>
